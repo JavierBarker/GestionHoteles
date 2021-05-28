9122aa2e94507cbaf86fa2e33646ba5bb2af5f99
Javier Barker <hernandez.javier2003@gmail.com>
2021-05-27 21:19:40 -0600

    Verificacion de Fechas
    se agrego la verificacion de fechas disponibles en el reservar habitacion

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -11,29 +11,47 @@ function reserveRoom(req, res){
     var modelReservation = new Reservation();
     if(req.user.role === 'ROL_USER'){
         if (params.entryDate && params.exitDate) {
-            
-            Hotel.findOne({_id: hotelId, "rooms._id": roomId}, {"rooms.$": 1}, (err, foundRoom)=>{
-                modelReservation.clientId = req.user.sub;
-                modelReservation.hotelId = hotelId;
-                modelReservation.room = {
-                    roomId: roomId,
-                    name: foundRoom.rooms[0].nameRoom,
-                    description: foundRoom.rooms[0].descriptionRoom,
-                    services: foundRoom.rooms[0].services,
-                    cost: foundRoom.rooms[0].cost,
-                    entryDate: params.entryDate,
-                    exitDate: params.exitDate
-                };
-                modelReservation.total = foundRoom.rooms[0].cost;
-                modelReservation.cancel = false;
-                Hotel.findByIdAndUpdate(hotelId, {$inc:{requests: 1}},{new: true, useFindAndModify: false},(err, incRequest)=>{
-                    modelReservation.save((err, savedReservation) => {
-                        if (err) return res.status(500).send({message: "Error al hacer la reservacion", err});
-                        if (!savedReservation)return res.status(500).send({message: "No se hizo la reservacion"});
-                        return res.status(200).send({savedReservation});
+            Reservation.find({"room.roomId": roomId}, (err, foundReservations)=>{
+                //return res.status(200).send({foundReservations});
+                var entryDate = new Date(params.entryDate);
+                var exitDate = new Date(params.exitDate);
+                
+                for (let index = 0; index < foundReservations.length; index++) {
+                    if (entryDate.getTime() >= foundReservations[index].room.entryDate.getTime() && entryDate.getTime() <= foundReservations[index].room.exitDate.getTime()) {
+                        return res.status(500).send({message: "esta fecha de entrada no esta disponible"});
+                    }
+                    
+                }
+                for (let index = 0; index < foundReservations.length; index++) {
+                    if (exitDate.getTime() >= foundReservations[index].room.entryDate.getTime() && exitDate.getTime() <= foundReservations[index].room.exitDate.getTime()) {
+                        return res.status(500).send({message: "esta fecha de salida no esta disponible"});
+                    }
+                    
+                }
+                Hotel.findOne({_id: hotelId, "rooms._id": roomId}, {"rooms.$": 1}, (err, foundRoom)=>{
+                    modelReservation.clientId = req.user.sub;
+                    modelReservation.hotelId = hotelId;
+                    modelReservation.room = {
+                        roomId: roomId,
+                        name: foundRoom.rooms[0].nameRoom,
+                        description: foundRoom.rooms[0].descriptionRoom,
+                        services: foundRoom.rooms[0].services,
+                        cost: foundRoom.rooms[0].cost,
+                        entryDate: entryDate,
+                        exitDate: exitDate
+                    };
+                    modelReservation.total = foundRoom.rooms[0].cost;
+                    modelReservation.cancel = false;
+                    Hotel.findByIdAndUpdate(hotelId, {$inc:{requests: 1}},{new: true, useFindAndModify: false},(err, incRequest)=>{
+                        modelReservation.save((err, savedReservation) => {
+                            if (err) return res.status(500).send({message: "Error al hacer la reservacion", err});
+                            if (!savedReservation)return res.status(500).send({message: "No se hizo la reservacion"});
+                            return res.status(200).send({savedReservation});
+                        })
                     })
                 })
             })
+            /**/
         }else{
         return res.status(500).send({message: 'Rellene los Espacios Necesarios'});
         }
